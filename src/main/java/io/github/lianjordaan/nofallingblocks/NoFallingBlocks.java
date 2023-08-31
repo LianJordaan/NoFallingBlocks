47a13bbba17c12f7a2d171fa2c53af986d5f2621
LianJordaan <lianjordaan30052005@gmail.com>
2023-08-31 14:10:23 +0200

    Implemented code to stop falling blocks

--- src/main/java/io/github/lianjordaan/nofallingblocks/NoFallingBlocks.java
+++ src/main/java/io/github/lianjordaan/nofallingblocks/NoFallingBlocks.java
@@ -1,17 +1,28 @@
 package io.github.lianjordaan.nofallingblocks;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class NoFallingBlocks extends JavaPlugin {
+public final class NoFallingBlocks extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // Register the event listener
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+        if (event.getEntityType() == org.bukkit.entity.EntityType.FALLING_BLOCK) {
+            // Cancel the event to prevent the block from falling
+            event.setCancelled(true);
+        }
     }
 }
